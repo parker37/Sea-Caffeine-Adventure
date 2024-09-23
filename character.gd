96d1a2d03393767dab85e2eba531b1ace6fb5748
coelslager <157177563+coelslager@users.noreply.github.com>
2024-09-22 21:26:41 -0400

    added some dialogue

---
+++ character.gd
@@ -0,0 +1,8 @@
+extends Sprite2D
+
+
+func _input(event):
+	if (event is InputEventMouseButton and event.is_released()):
+		if get_rect().has_point(to_local(event.position)):
+			#go to next room
+			DialogueManager.show_example_dialogue_balloon(load("res://dialogue/test_dialogue.dialogue"), "start")
